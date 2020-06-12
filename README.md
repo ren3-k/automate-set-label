b72748ca4a259698e24ec6f84a3a075732648c3c
ren3-k <abwh890909@gmail.com>
2020-06-12 20:51:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# automate-set-label
---
+++ automate_set_label.py
@@ -0,0 +1,32 @@
+from os import environ
+from github import Github
+
+ACCESS_TOKEN        = environ.get("ACCESS_TOKEN")
+GITHUB_REPOSITORY   = environ.get("GITHUB_REPOSITORY") 
+PULL_REQUEST_NUMBER = int(environ.get("PULL_REQUEST_NUMBER"))
+
+g = Github(ACCESS_TOKEN)
+repo = g.get_repo(GITHUB_REPOSITORY)
+pr = repo.get_pull(PULL_REQUEST_NUMBER)
+
+# ラベル名
+awaiting_review   = "レビュー待ち"
+awaiting_rereview = "再レビュー待ち"
+point_of_order    = "指摘事項あり"
+
+labels = pr.get_labels()
+label_num = labels.totalCount
+
+# PR時のラベル名を取得
+if label_num == 0:
+    label_name = ""
+else:
+    label_name = labels[0].name
+
+# ラベルを設定
+if label_name == "":
+    pr.add_to_labels(awaiting_review)
+elif label_name == point_of_order:
+    pr.remove_from_labels(point_of_order)
+    pr.add_to_labels(awaiting_rereview)
+
